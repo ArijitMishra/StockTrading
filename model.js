55ae0b19877eb3c8b07fef8194026f3ae7983fb2
gourcool <athneregourav98@gmail.com>
2021-03-18 10:34:24 +0530

    Update model.js

--- model.js
+++ model.js
@@ -1,28 +1,31 @@
-const mongoose = require('mongoose');
-require("dotenv/config");
-			mongoose.connect(process.env.DB_CONNECTION_STRING, {
-				useCreateIndex:true,
-				useNewUrlParser:true,
-				useUnifiedTopology:true
-			}).then(()=>{
-			console.log("Success of connection!!");
-			}).catch((e)=>{
-			console.log("Connection Failed!!");
-			});
-		
-		var registerSchema = mongoose.Schema({
-			Name: String,
-			Gender: String,
-			Contact: Number,
-			Dob:String,
-			Account:String,
-			Address:String,
-			Email:{
-				type:String,
-				unique:true
-			},
-			Password:String
+const mongoose = require('mongoose');  //importing mongoose module
+require("dotenv/config");   //importing .env file
+
+//Connection to MongoDB Atlas thorugh Connection String
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+			useCreateIndex:true,
+			useNewUrlParser:true,
+			useUnifiedTopology:true
+		}).then(()=>{
+		console.log("Success of connection!!"); //If Connection to MongoDB Atlas is Successful
+		}).catch((e)=>{
+		console.log("Connection Failed!!"); //If Connection failed shows error 
 		});
+		
+//Schema for the mongoose connection
+var registerSchema = mongoose.Schema({
+		Name: String,
+		Gender: String,
+		Contact: Number,
+		Dob:String,
+		Account:String,
+		Address:String,
+		Email:{   
+			type:String,
+			unique:true
+		},
+		Password:String
+	});
  
-var Registration = mongoose.model("Registration", registerSchema);
-module.exports = Registration;
+var Registration = mongoose.model("Registration", registerSchema); //Creating Model with our Schema and giving name Registration to it
+module.exports = Registration; //Exporting Model 
